0716e4b2916a807561f8a8137e13b2ab748678c5
Pavithra030297 <pavithrapavizh0302@gmail.com>
2024-05-29 18:18:01 +0530

    pavi added a file

---
+++ src/org/Blue/Client.java
@@ -0,0 +1,18 @@
+package org.Blue;
+import org.yellow.Company;
+
+public class Client {
+	public void address()
+	{
+		System.out.println("avadi");
+		
+	}
+
+	public static void main(String[] args) {
+		Client c=new Client();
+		c.address();
+		
+		Company c1=new Company();
+		c1.clientName();
+	}
+}
---
+++ src/org/yellow/Company.java
@@ -0,0 +1,11 @@
+package org.yellow;
+
+public class Company {
+	
+	public void clientName()
+	{
+		System.out.println("pavithra");
+		
+	}
+
+}
